fc0444124bec522edf769132af04d3a9fa01e4b6
IamDushu <108425089+IamDushu@users.noreply.github.com>
2022-07-26 21:19:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passOne = document.getElementById("passOneEl")
+let passTwo = document.getElementById("passTwoEl")
+
+
+function generate() { 
+    let passwordOne = ""
+    let passwordTwo = "" 
+    for(let i = 0; i<15; i++){
+        k = Math.floor(Math.random()*characters.length)
+        passwordOne += characters[k] 
+        passOne.textContent = passwordOne    
+    }
+    for(let i = 0; i<15; i++){
+        k = Math.floor(Math.random()*characters.length)
+        passwordTwo += characters[k] 
+        passTwo.textContent = passwordTwo   
+    }  
+}
+
+
